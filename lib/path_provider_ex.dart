620bc6f6cfdf42c2b83d9eebf50e0bcee404ca6a
Yoav Rofé <yoav@amudanan.co.il>
2020-02-05 10:41:52 +0200

    Update path_provider_ex.dart

--- lib/path_provider_ex.dart
+++ lib/path_provider_ex.dart
@@ -23,13 +23,16 @@ class PathProviderEx {
 
 class StorageInfo {
   final String appFilesDir;
-  final String rootDir;
   final int availableBytes;
   int get availableGB => availableBytes ~/ pow(2, 30);
   int get availableMB => availableBytes ~/ pow(2, 20);
-  StorageInfo(this.appFilesDir, this.rootDir, this.availableBytes);
+  
+  String get rootDir => appFilesDir.split("/").sublist(0,appFilesDir.split("/").length-4).join("/");
+  
+  StorageInfo(this.appFilesDir, this.availableBytes);
+  
   factory StorageInfo.fromJson(Map json) {
-    return StorageInfo(json["path"], json["rootPath"], json["availableBytes"]);
+    return StorageInfo(json["path"], json["availableBytes"]);
   }
   //get root with: reply[0]["path"].split("/").sublist(0,reply[0]["path"].split("/").length-4).join("/");
 }
